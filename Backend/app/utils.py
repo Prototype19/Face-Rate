e3bab579d283bc1b0beb1237e99e3780f6ec71ea
Prototype19 <daron.lebaredian@gmail.com>
2025-07-30 05:15:29 -0400

    Starting on the fastapi portion

--- Backend/app/utils.py
+++ Backend/app/utils.py
@@ -16,12 +16,33 @@ def create_model():
     print(f"[INFO] Created new {model.name} model.")
     return model
 
+# Create a model
+model = create_model()
+
 # Load the model
 def load_model(model_path: str = "models/final_model.pth"):
     model = create_model()
     model.load_state_dict(torch.load(model_path, weights_only=True))
-    # set model into eval mode 
     model.eval()
-    return model
+    return model.to(DEVICE)
 
-# Create predict function
+# Preprocessing transformation pipeline
+preprocess = transforms.Compose([
+    transforms.Resize(160),
+    transforms.CenterCrop(160),
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale=True),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Convert image file into tensor
+def bytes_to_tensor(data: bytes) -> torch.Tensor:
+    img = Image.open(BytesIO(data)).convert("RGB")
+    return preprocess.unsqueeze(0).to(DEVICE)
+
+# Prediction function
+@torch.inference_mode()
+def predict(img_bytes: bytes) -> dict:
+    x = bytes_to_tensor(img_bytes)
+    prediction = model(x).item()
+    return prediction
